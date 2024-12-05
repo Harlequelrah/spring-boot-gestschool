350675a992a5af6a2c2323f8778e0028f5060661
Harlequelrah <degbovimax19@gmail.com>
2024-12-05 23:55:01 +0000

    creation des routes pour la création et la sauvegarde d'etudiant

--- src/main/java/com/demo/demo/controllers/StudentController.java
+++ src/main/java/com/demo/demo/controllers/StudentController.java
@@ -1,10 +1,14 @@
 package com.demo.demo.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.demo.models.Student;
 import com.demo.demo.services.StudentService;
+
 
 @RestController
 public class StudentController {
@@ -15,9 +19,17 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping("/save-student")
-    public Student saveStudent(Student student) {
-        return studentService.saveStudent(student);
+    @PostMapping("/students/save-student")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students/create-student?success";
     }
+
+    @GetMapping("/students/create-student")
+    public String createStudent(Model model) {
+        model.addAttribute("student", new Student());
+        return "create_student_form";
+    }
+
 
 }
