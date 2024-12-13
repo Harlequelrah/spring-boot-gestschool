ea5afee3b760ed460caa19419fe8eda73d37eb10
Harlequelrah <degbovimax19@gmail.com>
2024-12-13 14:19:03 +0000

    mise à jour du code

--- src/main/java/com/demo/demo/controllers/StudentController.java
+++ src/main/java/com/demo/demo/controllers/StudentController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.demo.demo.models.Student;
 import com.demo.demo.services.StudentService;
@@ -20,17 +19,17 @@ public class StudentController {
     private  StudentService studentService;
 
     @PostMapping("/students/save-student")
-    // public String saveStudent(@ModelAttribute("student") Student student) {
-    public String saveStudent(@RequestBody Student student) {
+    public String saveStudent(@ModelAttribute("student") Student student) {
+    // public String saveStudent(@RequestBody Student student) {
         studentService.saveStudent(student);
-        return "redirect:/students/create-student?success";
+        return "redirect:/students";
     }
 
 
     @GetMapping("/students/create-student")
     public String createStudent(Model model) {
         model.addAttribute("student", new Student());
-        return "create_student_form";
+        return "create";
     }
     @GetMapping("/students")
     public String getStudents(Model model) {
